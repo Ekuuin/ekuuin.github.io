892a4b95ba9a5c82d6f68a267010ad6701d2b081
Ekuuin <71789461+Ekuuin@users.noreply.github.com>
2023-11-29 22:57:03 -0600

    Everything OK, animations pending

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
-    <v-footer>
-        
+    <v-footer height="80px" style="background: transparent; padding: 70px 25px 25px 0; z-index: 1004;">
+        <v-spacer></v-spacer>
+        <span class="text-footer">&#169; Creado por Edwin Valdés. {{ year }}.</span>
     </v-footer>
 </template>
 
@@ -9,5 +10,8 @@ const year = new Date().getFullYear();
 </script>
 
 <style>
-    
+    .text-footer{
+        font-family: 'Open Source Pro', monospace;
+        font-size: 1rem;
+    }
 </style>
